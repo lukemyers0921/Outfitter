6ad507f9c97bcb12aa30e08fa4c0c8d914521c9a
Luke Myers <lukemyers0921@gmail.com>
2018-05-12 13:18:05 -0400

    added 3 models clothing_item, users, and outfits

---
+++ models/outfits.js
@@ -0,0 +1,8 @@
+module.exports = function(sequelize, DataTypes) {
+    var Outfit = sequelize.define("Outfit", {
+      top: DataTypes.STRING, // equal to id of clothing_item
+      bottom: DataTypes.STRING,  // ^
+      shoes: DataTypes.STRING, // ^
+    });
+    return Outfit;
+  };
