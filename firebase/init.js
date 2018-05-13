0ea48ee167c31f6f83880c22cfcfaee884574781
Luke Myers <lukemyers0921@gmail.com>
2018-05-13 15:24:53 -0400

    added firebase stuff

--- firebase/init.js
+++
@@ -1,9 +0,0 @@
-    var config = {
-        apiKey: "AIzaSyC9V3KYVVj2rt4HxZf-93veFlGLQ38hOkk",
-        authDomain: "outfitter-a3fdd.firebaseapp.com",
-        databaseURL: "https://outfitter-a3fdd.firebaseio.com",
-        projectId: "outfitter-a3fdd",
-        storageBucket: "",
-        messagingSenderId: "834658064656"
-      };
-      firebase.initializeApp(config);
