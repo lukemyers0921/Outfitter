8b702f3124f1c64c5d901cd28f8707b53505dace
Luke Myers <lukemyers0921@gmail.com>
2018-05-13 08:43:42 -0400

    my branch??

---
+++ firebase/init.js
@@ -0,0 +1,9 @@
+    var config = {
+        apiKey: "AIzaSyC9V3KYVVj2rt4HxZf-93veFlGLQ38hOkk",
+        authDomain: "outfitter-a3fdd.firebaseapp.com",
+        databaseURL: "https://outfitter-a3fdd.firebaseio.com",
+        projectId: "outfitter-a3fdd",
+        storageBucket: "",
+        messagingSenderId: "834658064656"
+      };
+      firebase.initializeApp(config);
