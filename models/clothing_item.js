e0926d0c8c753b4a3cae56e28c1912c724b1b693
Lacie Forrest <lvforrest@yahoo.com>
2018-05-16 20:24:56 -0400

    update

--- models/clothing_item.js
+++ models/clothing_item.js
@@ -3,11 +3,8 @@ module.exports = function(sequelize, DataTypes) {
   
     var Clothing_item = sequelize.define("Clothing_item", {
       user_id: DataTypes.STRING,
-      clean: DataTypes.BOOLEAN,
       type: DataTypes.STRING,
-      primary_color: DataTypes.STRING,
-      accent_color: DataTypes.STRING,
-      pattern: DataTypes.STRING,
+      img_url: DataTypes.STRING,
     });
     Clothing_item.associate = function(models){
       Clothing_item.belongsTo(models.Outfit,{
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -40,4 +40,72 @@ module.exports = function(app) {
       res.json(results);
     });
   });
+  
+  app.get("/api/outfit", function(req, res) {
+    // findAll returns all entries for a table when used with no options
+    db.Outfit.findAll({
+      include: [db.Clothing_item]
+    }).then(function(dbOutfit) {
+      // We have access to the outfit as an argument inside of the callback function
+      res.json(dbOutfit);
+    });
+  });
+  app.get("/api/outfit", function(req, res) {
+    // findAll returns all entries for a table when used with no options
+    db.Outfit.findOne({
+      where: {
+        user_id: req.params.user_id
+      },
+      include: [db.Clothing_item]
+    }).then(function(dbOutfit) {
+      // We have access to the outfit as an argument inside of the callback function
+      res.json(dbOutfit);
+    });
+  });
+  // POST route for saving a new Outfits
+  app.post("/api/outfit", function(req, res) {
+    // create takes an argument of an object describing the item we want to
+    // insert into our table. In this case we just we pass in an object with a text
+    // and complete property
+    db.Outfit.create({
+      user_id: req.body.user_id,
+      top: req.body.top,
+      bottom: req.body.bottom,
+      shoes: req.body.shoes,
+    }).then(function(dbOutfit) {
+      // We have access to the new Outfits as an argument inside of the callback function
+      res.json(dbOutfit);
+    });
+  });
+  // DELETE route for deleting outfit. We can get the id of the Outfits to be deleted from
+  // req.params.id
+  app.delete("/api/outfit/:id", function(req, res) {
+    // We just have to specify which Outfits we want to destroy with "where"
+    db.Outfit.destroy({
+      where: {
+        user_id: req.params.user_id
+      },
+      include:[db.Clothing_item]
+    }).then(function(dbOutfit) {
+      res.json(dbOutfit);
+    });
+  });
+  // PUT route for updating outfit. We can get the updated Outfits data from req.body
+  app.put("/api/outfit", function(req, res) {
+    // Update takes in an object describing the properties we want to update, and
+    // we use where to describe which objects we want to update
+    db.Outfit.update({
+      top: req.body.top,
+      bottom: req.body.bottom,
+      shoes: req.body.shoes,
+    }, {
+      where: {
+        user_id: req.params.user_id
+      },
+      include: [db.Clothing_item]
+    }).then(function(dbOutfit) {
+      res.json(dbOutfit);
+    });
+  });  
+
 };
